6ab1758a70273451d2e606ff03bcb237461e852f
Jose Flavio A <joseflavio-at-gmail-dot-com>
2017-03-21 22:11:35 +0100

    Displaying the response body

--- http-client-test/src/main.rs
+++ http-client-test/src/main.rs
@@ -1,10 +1,14 @@
 extern crate hyper;
 
+use std::io::Read;
 use hyper::Client;
 
 fn main() {
     let client = Client::new();
-    let res = client.get("http://www.addic7ed.com/").send().unwrap();
+    let mut res = client.get("http://www.addic7ed.com/").send().unwrap();
     assert_eq!(res.status, hyper::Ok);
     println!("< status code: {}", res.status);
+    let mut s = String::new();
+    res.read_to_string(&mut s).unwrap();
+    println!("{}", s);
 }
